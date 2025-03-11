aa153ecab07bbe9915410afaa2d25d20cd141aa6
Alejandro Parrado Di Doménico <alejopadido13@gmail.com>
2025-03-11 05:28:52 -0500

    Add text button & haptic feedback to filled

--- src/top_app/lib/shared/widgets/buttons/white_filled_button.dart
+++ src/top_app/lib/shared/widgets/buttons/white_filled_button.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:top_app/core/theme/app_colors.dart';
 import 'package:top_app/core/theme/app_texts_styles.dart';
 
 class WhiteFilledButton extends StatelessWidget {
-  const WhiteFilledButton({super.key, required this.text, required this.onPressed});
+  const WhiteFilledButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.hasHaptic = true,
+  });
 
   final String text;
   final void Function() onPressed;
+  final bool hasHaptic;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: () {
+        if (hasHaptic) {
+          HapticFeedback.lightImpact();
+        }
+        onPressed();
+      },
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.whitePrimary,
         foregroundColor: AppColors.blackPrimary,
