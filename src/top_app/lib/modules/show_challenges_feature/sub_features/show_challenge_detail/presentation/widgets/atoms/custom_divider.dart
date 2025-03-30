250581949fbf6a44e4339bc3462edabb3fa31428
Alejandro Parrado Di Doménico <alejopadido13@gmail.com>
2025-03-30 14:42:53 -0500

    Fix: Create re-usable widget & fix spacings

---
+++ src/top_app/lib/modules/show_challenges_feature/sub_features/show_challenge_detail/presentation/widgets/atoms/custom_divider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:top_app/core/theme/app_colors.dart';
+
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({
+    super.key,
+    this.spacing = 15,
+  });
+
+  final double spacing;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: spacing),
+        const Divider(color: AppColors.grayDark),
+        SizedBox(height: spacing),
+      ],
+    );
+  }
+}
