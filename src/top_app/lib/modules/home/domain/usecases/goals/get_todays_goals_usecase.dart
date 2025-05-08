577f92d386476cc37937d722a6f9e6d616906bae
Alejandro Parrado Di Doménico <alejopadido13@gmail.com>
2025-05-08 09:56:09 -0500

    chore: Add type notations

--- src/top_app/lib/modules/home/domain/usecases/goals/get_todays_goals_usecase.dart
+++ src/top_app/lib/modules/home/domain/usecases/goals/get_todays_goals_usecase.dart
@@ -4,8 +4,8 @@ import 'package:top_app/shared/entities/templates/goal.dart';
 @injectable
 class GetTodaysGoalsUsecase {
   List<Goal> call(List<Goal> goals) {
-    final today = DateTime.now();
-    return goals.where((goal) {
+    final DateTime today = DateTime.now();
+    return goals.where((Goal goal) {
       return goal.dueDate.year == today.year &&
           goal.dueDate.month == today.month &&
           goal.dueDate.day == today.day;
